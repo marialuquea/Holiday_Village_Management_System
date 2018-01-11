382e344a79b15c75d029cc3010964c44916dbb1b
Maria Luque Anguita <40280156@live.napier.ac.uk>
2018-01-11 14:45:18 +0000

    Add files via upload

---
+++ Data/ObjectsList.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using Business;
+
+namespace Data
+{
+    /*
+     * Author: Maria Luque Anguita 40280156
+     * Description of class: methods to read and save objects to lists
+     * Date last modified: 3/12/2017
+     * Dessign pattern: Singleton
+     */
+    public class ObjectsList
+    {
+        //lists that will store objects
+        private List<Customer> custList = new List<Customer>();
+        private List<Guest> guestList = new List<Guest>();
+        private List<Booking> bookingList = new List<Booking>();
+
+        //singleton 
+        private static ObjectsList instance;
+
+        //singleton constructor
+        private ObjectsList() { }
+
+        public static ObjectsList Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ObjectsList();
+                }
+                return instance;
+            }
+        }
+
+        //Add
+        public void addCustomer(Customer cust)
+        {
+            custList.Add(cust);
+        }
+        
+        //Find
+        public Guest findGuest(string passport)
+        {
+            foreach (Guest g in guestList)
+            {
+                if (passport == g.PassportNo)
+                {
+                    return g;
+                }
+            }
+            return null;
+        }
+
+        public Booking findBooking(int id)
+        {
+            foreach (Booking b in bookingList)
+            {
+                if (id == b.BookingID)
+                {
+                    return b;
+                }
+            }
+            return null;
+        }
+
+        public Customer findCustomer(int id)
+        {
+            foreach (Customer c in custList)
+            {
+                if (id == c.CustomerID)
+                {
+                    return c;
+                }
+            }
+            return null;
+        }
+
+        //Delete
+        public void deleteCustomer(int reference)
+        {
+            Customer c = this.findCustomer(reference);
+            if (c != null)
+            {
+                custList.Remove(c);
+            }
+        }
+
+        public void deleteBooking(int id)
+        {
+            Booking b = this.findBooking(id);
+            if (b != null)
+            {
+                bookingList.Remove(b);
+            }
+        }
+
+        //Return complete lists
+        public List<Booking> allBookings
+        {
+            get { return bookingList; }
+            set { bookingList = value; }
+        }
+
+        public List<Customer> allCustomers
+        {
+            get { return custList; }
+            set { custList = value; }
+        }
+
+    }
+}
---
+++ Data/Serializer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using Business;
+
+namespace Data
+{
+    /*
+     * Author: Maria Luque Anguita 40280156
+     * Description of class: Serialise lists to xml files and deserialise
+     * Date last modified: 3/12/2017
+     */
+    [Serializable()]
+    public class Serializer
+    {   
+        //Access the singleton instance of the lists
+        ObjectsList dataLayerSingleton = ObjectsList.Instance;
+        
+        //serialise lists (save informatin to xml file)
+        public void bookingsSerializer()
+        {
+            // Create a new XmlSerializer instance with the type of the test class
+            XmlSerializer SerializerObj = new XmlSerializer(typeof(List<Booking>));
+
+            // Create a new file stream to write the serialized object to a file
+            TextWriter WriteFileStream = new StreamWriter(@"C:\Users\Maria\Documents\2nd year\Object Oriented Software Development\cw2(2)\bookings.xml");
+
+            //write object into file
+            SerializerObj.Serialize(WriteFileStream, dataLayerSingleton.allBookings);
+
+            // Cleanup
+            WriteFileStream.Close();
+        }
+
+        public void customerSerializer()
+        {
+            // Create a new XmlSerializer instance with the type of the test class
+            XmlSerializer SerializerObj = new XmlSerializer(typeof(List<Customer>));
+
+            // Create a new file stream to write the serialized object to a file
+            TextWriter WriteFileStream = new StreamWriter(@"C:\Users\Maria\Documents\2nd year\Object Oriented Software Development\cw2(2)\customerS.xml");
+                
+            //write object into file
+            SerializerObj.Serialize(WriteFileStream, dataLayerSingleton.allCustomers);
+
+            // Cleanup
+            WriteFileStream.Close();
+            
+        }
+
+        //deserialize lists (read information from xml file)
+        public void bookingDeserializer()
+        {
+            //Load the object
+
+            //Create a new XmlSerializer with the type of the test class
+            XmlSerializer SerializerObj = new XmlSerializer(typeof(List<Booking>));
+
+            // Create a new file stream for reading the XML file
+            FileStream ReadFileStream = new FileStream(@"C:\Users\Maria\Documents\2nd year\Object Oriented Software Development\cw2(2)\bookings.xml", FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            // Load the object saved above by using the Deserialize function
+            List<Booking> loadedObj = (List<Booking>)SerializerObj.Deserialize(ReadFileStream);
+
+            dataLayerSingleton.allBookings = loadedObj;
+
+            // Cleanup
+            ReadFileStream.Close();
+        }
+
+        public void customerDeserializer()
+        {
+            //Load the object
+
+            //Create a new XmlSerializer with the type of the test class
+            XmlSerializer SerializerObj = new XmlSerializer(typeof(List<Customer>));
+
+            // Create a new file stream for reading the XML file
+            FileStream ReadFileStream = new FileStream(@"C:\Users\Maria\Documents\2nd year\Object Oriented Software Development\cw2(2)\customerS.xml", FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            // Load the object saved above by using the Deserialize function
+            List<Customer> loadedObj = (List<Customer>)SerializerObj.Deserialize(ReadFileStream);
+
+            dataLayerSingleton.allCustomers = loadedObj;
+
+            // Cleanup
+            ReadFileStream.Close();
+
+            
+        }
+    
+    }
+}
